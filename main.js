981e604c83997e33b976fe3d17a956553fed0c26
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-09-22 15:56:15 +0500

    Change language to english

--- main.js
+++ main.js
@@ -9,32 +9,32 @@ const randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
 
 userChance = 6;
-textCounter.textContent = `Qolgan urunishlar soni : ${userChance}`;
+textCounter.textContent = `Number of remaining attempts : ${userChance}`;
 
 formRandom.addEventListener("submit", (evt) => {
   evt.preventDefault();
   userChance--;
-  textCounter.textContent = `Qolgan urunishlar soni : ${userChance}`;
+  textCounter.textContent = `Number of remaining attempts : ${userChance}`;
+
   formInputValue = Number(formInput.value);
-  //   console.log(formInputValue);
   if (formInputValue === 0) {
     randomResText.classList.add("text-danger", "bg-info");
-    randomResText.textContent = `Son kiriting`;
-    userChance = 7;
+    randomResText.textContent = `Enter a number`;
+    userChance = 6;
   } else if (userChance === 0) {
     formInput.disabled = true;
     randomBtn.setAttribute("disabled", "Stop");
     randomResText.classList.add("text-white", "bg-danger");
-    randomResText.textContent = `Afsuski sizning imkoniyatingiz qolmadi qaytadan urunib ko'ring`;
+    randomResText.textContent = `Unfortunately, your chance has run out. Please try again`;
   } else if (randomNumber > formInputValue) {
     randomResText.classList.add("text-danger", "bg-info");
-    randomResText.textContent = `Siz topgan sondan katta`;
+    randomResText.textContent = `Bigger than the number you found`;
   } else if (randomNumber < formInputValue) {
     randomResText.classList.add("text-danger", "bg-info");
-    randomResText.textContent = `Siz topgan sondan kichik`;
+    randomResText.textContent = `Smaller than the number you found`;
   } else if (formInputValue === randomNumber) {
     randomResText.classList.add("text-white", "bg-success");
-    randomResText.textContent = `Siz g'olib bo'ldingiz. Tabriklaymiz`;
+    randomResText.textContent = `Congratulations. You won.`;
     formInput.disabled = true;
     randomBtn.setAttribute("disabled", "Stop");
   }
